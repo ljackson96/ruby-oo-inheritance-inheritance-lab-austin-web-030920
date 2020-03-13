8922640da5ba9a1deeb4f9e018e9740c0db9e7b7
l.jackson96 <l.jackson96@outlook.com>
2020-03-13 13:35:53 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+    attr_accessor :first_name, :last_name, :knowledge
+
+    def initialize
+        knowledge = []
+        @knowledge = knowledge
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
 
 end
